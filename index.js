74166b93460413b5ef7dbeb4c7fac7cae3ad34d2
jackseni1998 <jackseniD@gmail.com>
2019-08-28 16:55:59 -0600

    first commit

--- index.js
+++ index.js
@@ -5,23 +5,24 @@ const express = require('express');
 const App = express()
 
 // Definimos algunas variables que usaremos en las distintas funciones
-const port = 3000
+const port = process.env.PORT || 3000
 const options = {
     root: __dirname
-}
+};
+
 
 // Definimos funciones para luego usarlas al recibir una petición en el router
 function getHTML(req, res) {
     res.sendFile('./index.html', options, (err) => {
-        if (err) throw err
+        if (err) throw err;
         console.log('Sirviendo index.html')
-    });
-}
+    })
+};
 
 // Definimos las rutas
-App.get('/', getHTML)
+App.get('/', getHTML);
 
 // Escuchamos el puerto de Express
 App.listen(port, () => {
     console.log('Aplicacion escuchando en el puerto: ' + port)
-})
+});
